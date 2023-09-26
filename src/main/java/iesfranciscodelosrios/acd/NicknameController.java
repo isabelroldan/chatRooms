2e84ef95a259db15956d18e6d6002539cb2c6163
Isabel <isabelroldancordoba@hotmail.com>
2023-09-26 14:54:31 +0200

    prueba

--- src/main/java/iesfranciscodelosrios/acd/NicknameController.java
+++ src/main/java/iesfranciscodelosrios/acd/NicknameController.java
@@ -12,5 +12,24 @@ public class NicknameController {
     @FXML
     private Button entrarButton;
 
-    // Aquí puedes agregar la lógica para manejar el botón "ENTRAR" y el nickname
+    public void handleEntrarButton() {
+        String nickname = nicknameTextField.getText();
+        // Envía el nickname al servidor para su verificación
+        boolean isNicknameAvailable = verificarNicknameEnServidor(nickname);
+        if (isNicknameAvailable) {
+            // El nickname es válido, permitir acceso a la sala
+            // Puedes abrir la ventana de la sala o realizar la acción correspondiente aquí
+        } else {
+            // El nickname ya está en uso, muestra un mensaje de error al usuario
+            System.out.println("El nickname ya está en uso. Elige otro.");
+            // Puedes mostrar un mensaje de error en la interfaz de usuario si lo deseas
+        }
+    }
+
+    // Método para enviar el nickname al servidor y verificar su disponibilidad
+    private boolean verificarNicknameEnServidor(String nickname) {
+        // Lógica para verificar si el nickname ya está en uso en el servidor
+        // Debes implementar esto en tu servidor
+        return true; // Devuelve true si el nickname está disponible, false si no lo está
+    }
 }
