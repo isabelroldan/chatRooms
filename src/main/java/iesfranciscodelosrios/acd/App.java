5575b443f213bc00316f4240ea506892d928f227
Alejandro <gutierrezcrucesalejandro@gmail.com>
2023-09-27 11:11:29 +0200

    Cambios de index y Nickname proyecto arranca

--- src/main/java/iesfranciscodelosrios/acd/App.java
+++ src/main/java/iesfranciscodelosrios/acd/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("nickname"), 640, 480);
+        scene = new Scene(loadFXML("index"), 1440, 900);
         stage.setScene(scene);
         stage.show();
     }
