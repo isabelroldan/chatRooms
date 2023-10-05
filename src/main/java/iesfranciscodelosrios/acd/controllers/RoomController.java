e396186635bf650266f75b1b23bc28e88841fac4
Isabel <isabelroldancordoba@hotmail.com>
2023-10-06 00:51:10 +0200

    Número de sala, nickname recuperados,a la espera de xml bien formado para recuperar numero de usuarios en sala

--- src/main/java/iesfranciscodelosrios/acd/controllers/RoomController.java
+++ src/main/java/iesfranciscodelosrios/acd/controllers/RoomController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -22,9 +24,10 @@ public class RoomController {
     @FXML
     private Label numPersonasLabel;
     @FXML
-    private Label numeroSalaLabel; // Añade una etiqueta en tu vista para mostrar el número de la sala
+    private Label numeroSalaLabel;
 
     private int numeroSala; // Agrega este atributo
+    private static final String RUTA_XML_USUARIOS = "src/main/resources/iesfranciscodelosrios/acd/Xmls/Users.xml";
 
     private String nickname;
 
@@ -33,6 +36,14 @@ public class RoomController {
             // Actualizar la etiqueta con el texto del TextField
             mensajeTextField.setText(newValue);
         });
+
+
+        // Carga el archivo XML de usuarios y busca el número de personas en esta sala
+        int numeroPersonasEnSala = obtenerNumeroPersonasEnSala(numeroSala);
+
+        // Actualiza la vista con el número de personas en esta sala
+        numPersonasLabel.setText(String.valueOf(numeroPersonasEnSala));
+
     }
 
     public void setNickname(String nickname) {
@@ -40,24 +51,44 @@ public class RoomController {
     }
 
     public void setNumeroSala(int numeroSala) {
-        this.numeroSala = numeroSala;
+        numeroSalaLabel.setText(String.valueOf(numeroSala));
     }
 
-    private int obtenerNumeroDePersonasEnSala(int sala) {
+    private int obtenerNumeroPersonasEnSala(int numeroSala) {
         try {
-            File archivoXML = new File("ruta/a/tu/archivo.xml"); // Cambia la ruta al archivo XML
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document documento = dBuilder.parse(archivoXML);
+            // Cargar el archivo XML de usuarios
+            File file = new File(RUTA_XML_USUARIOS);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(file);
 
-            XPathFactory xpathFactory = XPathFactory.newInstance();
-            XPath xpath = xpathFactory.newXPath();
-            XPathExpression expr = xpath.compile("/users/user[room='" + sala + "']");
-            NodeList listaUsuarios = (NodeList) expr.evaluate(documento, XPathConstants.NODESET);
+            // Obtener la lista de nodos de usuario
+            NodeList userList = doc.getElementsByTagName("user");
 
-            return listaUsuarios.getLength();
+            // Inicializar el contador para el número de personas en la sala
+            int contadorPersonas = 0;
+
+            // Iterar a través de los nodos de usuario para contar aquellos en la sala especificada
+            for (int i = 0; i < userList.getLength(); i++) {
+                Node userNode = userList.item(i);
+                if (userNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element userElement = (Element) userNode;
+
+                    // Obtener el número de sala del nodo actual
+                    String salaUsuario = userElement.getElementsByTagName("room").item(0).getTextContent();
+
+                    // Verificar si esta sala coincide con la sala especificada
+                    if (Integer.parseInt(salaUsuario) == numeroSala) {
+                        contadorPersonas++;
+                    }
+                }
+            }
+
+            return contadorPersonas;
         } catch (Exception e) {
+            // Manejo de excepciones en caso de error al cargar el archivo XML, etc.
             e.printStackTrace();
+            // En caso de error, devolver 0 como número de personas
             return 0;
         }
     }
