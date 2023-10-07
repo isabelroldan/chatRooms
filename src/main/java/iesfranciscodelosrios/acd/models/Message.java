0e6383ad68ac1aa70a7e0e3f97684faebc3f2620
Isabel <isabelroldancordoba@hotmail.com>
2023-10-07 12:50:06 +0200

    Configuración de la clase mensaje con los atributos que necesitamos

--- src/main/java/iesfranciscodelosrios/acd/models/Message.java
+++ src/main/java/iesfranciscodelosrios/acd/models/Message.java
@@ -1,19 +1,18 @@
 package iesfranciscodelosrios.acd.models;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Date;
 
-public class Message {
+public class Message implements Serializable {
     private String nickname;
     private String content;
     private LocalDateTime timestamp;
-    private boolean isMine; // Indica si el mensaje es tuyo
 
-    public Message(String nickname, String content, boolean isMine) {
+    public Message(String nickname, String content, LocalDateTime timestamp) {
         this.nickname = nickname;
         this.content = content;
-        this.timestamp = LocalDateTime.now();
-        this.isMine = isMine;
+        this.timestamp = timestamp;
     }
 
     public String getNickname() {
